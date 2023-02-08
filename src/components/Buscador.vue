d7d56630b727988ef79b26bfd7f175ea76e4e879
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-08 20:46:11 +0100

    First

---
+++ src/components/Buscador.vue
@@ -0,0 +1,28 @@
+<template>
+  <section id="MiscaMuscaMiqueyMouse">
+      <input type="text" v-model="Manute" @change="ModoDiablo()">
+      <h1>{{Diabloz}}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        Manute: "",
+        Diabloz:""
+    }
+},
+methods:{
+    ModoDiablo: function(){
+        if(this.Manute==="negro"){
+            this.Diabloz="Putos negros"
+        }
+    }
+}
+}
+</script>
+
+<style>
+#MiscaMuscaMiqueyMouse input{border-style: solid;}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="mr">
+      <v-toolbar flat height="72">
+      <v-switch
+        v-model="$vuetify.theme.dark"
+        inset
+        label="Modo blanquista"
+        persistent-hint
+      ></v-switch>
+    </v-toolbar>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.theme--light.v-sheet{background-color: #726d6d50 !important;}
+</style>
